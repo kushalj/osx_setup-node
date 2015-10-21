72aa07d397846b1d12fbdbe685e3ef4fd88ff124
Kushal <kushal@elysian.net>
2015-10-21 20:32:22 +0100

    added push.default 'simple' to git_setup

--- git_setup.sh
+++ git_setup.sh
@@ -5,8 +5,14 @@ git config --global user.name $1
 git config --global user.email $2
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
 
-ssh-keygen -t rsa -C $2
-cat ~/.ssh/id_rsa.pub
+read -p "Generate SSH Key? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    ssh-keygen -t rsa -C $2
+    cat ~/.ssh/id_rsa.pub
+fi
 
 
